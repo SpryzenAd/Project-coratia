0e3e6e3907c07b61bfdd1789241de94ff54226c4
Adarsh Pattnaik <adarshme05102002@gmail.com>
2024-11-11 20:51:13 +0530

    Award section upated

--- config/content/Awards.js
+++ config/content/Awards.js
@@ -1,42 +1,74 @@
 export const awards = [
   {
-    text: "Winner of Digital Communication Innovation Square (DCIS)",
+    text: "Top-10 winner in Emerge 50 by NASSCOM",
+    year: "2024",
+  },
+  {
+    text: "Winner of USHUS Program by Cochin Shipyard Ltd.",
+    year: "2024",
+  },
+  {
+    text: "Winner of Boeing BUILD 3.0 by Boeing",
+    year: "2024",
+  },
+  {
+    text: "Featured in Shark Tank India S3 by SONY",
+    year: "2024",
+  },
+  {
+    text: "Most Promising Startup by ELCINA",
     year: "2023",
   },
   {
-    text: "Most Promising Startup by Electronics & semiconductor Associaton of India",
-    year: "2022",
-  },
-  {
-    text: "Selected in IICG program for co-development of sensors with C-MET",
+    text: "Winner of  DCIS 2.0 by Ministry of Telecom",
     year: "2023",
   },
   {
-    text: "Top-12 Qualcomm Design in India Challenge",
-    year: "2022",
-  },
-  {
-    text: "Winner of IMAGE COE in Computer Vision & AI",
+    text: "Winner of IMAGE-COE in Computer Vision & AI, by MeitY",
     year: "2023",
   },
   {
-    text: "Winner of NXP Tech Startup Challenge",
-    year: "2022",
-  },
-  {
-    text: "Top 10 in Sonipat Startup Summit",
-    year: "2022",
-  },
-  {
-    text: "Top 100 startups in UK- India Tech startup programme",
-    year: "2022",
-  },
-  {
-    text: "Best Startup of the Year by Electronic Industries Assosciation Of India",
+    text: "Recipient of NIDHI Prayas by DST",
     year: "2023",
   },
   {
-    text: "Winner of iDEX Challenge",
+    text: "Winner of MEITY TIDE 2.0 by MeitY Start Hub",
+    year: "2023",
+  },
+  {
+    text: "Winner of IICG Program, C-MET by MeitY & TATA Steel",
+    year: "2023",
+  },
+  {
+    text: "Winner of NRDC  Seed fund by Ministry of Science & Technology",
+    year: "2023",
+  },
+  {
+    text: "Best startup of the year by IESA",
     year: "2022",
+  },
+  {
+    text: "Top 10 in Sonipat Startup Summit by AIC IIT Delhi",
+    year: "2022",
+  },
+  {
+    text: "Winner of iDEX-DISC-7 challenge by Ministry of Defence",
+    year: "2022",
+  },
+  {
+    text: "Top-12 QDIC by Qualcomm",
+    year: "2022",
+  },
+  {
+    text: "Winner of Maharashtra Startup Week by MSInS",
+    year: "2022",
+  },
+  {
+    text: "Winner of NXP Tech Startup Challenge by Ministry of Commerce and Industry",
+    year: "2022",
+  },
+  {
+    text: "TOP 100 startups in UK-INDIA Tech startup program by British High Commission",
+    year: "2021",
   },
 ];
